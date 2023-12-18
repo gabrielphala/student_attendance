8143db4fbe050e56c6a5293d59e1a71b51c1ad8e
Gabriel Phala <gabrielphala@outlook.com>
2023-12-18 10:14:59 +0200

    updated numric fields to text

--- models/student.py
+++ models/student.py
@@ -4,6 +4,6 @@ class Student(models.Model):
 	_name = "attendance.student"
 	_description = "Students records"
 	name = fields.Char(string='name', required=True)
-	student_no = fields.Integer(string='Student no')
-	email_address = fields.Text(string='Email address')
-	gender = fields.Selection([('male','Male'), ('female', 'Female'), ('other', 'Other')], string='Gender')
+	student_no = fields.Char(string='Student no', required=True)
+	email_address = fields.Char(string='Email address', required=True)
+	gender = fields.Selection([('male','Male'), ('female', 'Female'), ('other', 'Other')], string='Gender', required=True)
